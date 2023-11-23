aee538e3fe76b5e8be1cd6d0325e3431b1b04d29
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2023-11-23 08:56:36 +0000

    remove legacy code

--- test/PriceFeedConsumer.t.sol
+++ test/PriceFeedConsumer.t.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.16;
 
 import {PriceFeedConsumer} from "../src/PriceFeedConsumer.sol";
 import {MockV3Aggregator} from "./mocks/MockV3Aggregator.sol";
--- test/utils/Cheats.sol
+++ test/utils/Cheats.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.16;
 
 abstract contract Cheats {
     // sets the block timestamp to x
