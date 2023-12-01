2fc26f497745673820b58137f7d5f62baaf2a5fb
Tikhon <tikhon@wevestr.com>
2023-12-01 22:54:07 +0100

    added reused interfaces

---
+++ demo/contracts/contracts/OracleInterfaces.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.16;
+
+enum FeedType {
+    DataStream,
+    PriceFeed
+}
+
+struct ForwardData {
+    int256 price;
+    FeedType feedType;
+    bytes forwardArguments;
+}
+
+interface IOracleConsumerContract {
+    function consume(ForwardData memory forwardData) external returns (bool);
+}
+
+interface IOracle {
+    function addRequest(
+        address callbackContract,
+        bytes memory callbackArgs
+    ) external returns (bool);
+}
