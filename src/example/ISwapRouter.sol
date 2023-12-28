610df9f5555bc417e76b27654087a2f427b3eece
Dzmitry Lahunouski <killroy192@gmail.com>
2023-12-28 14:57:15 +0000

    remove manual abicoder version setOC

--- src/example/ISwapRouter.sol
+++ src/example/ISwapRouter.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 pragma solidity ^0.8.16;
-pragma abicoder v2;
 
 import {IUniswapV3SwapCallback} from
     "@uniswap/v3-core/contracts/interfaces/callback/IUniswapV3SwapCallback.sol";
